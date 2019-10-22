b7a35e5b6aaf45758565124413787813a73b7baa
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-10-22 12:00:29 +0200

    More

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  reporters: [
-    'default',
-    [
-      'jest-junit',
-      {
-        classNameTemplate: '{classname}',
-        titleTemplate: '{title}',
-        addFileAttribute: 'true',
-      },
-    ],
-  ],
   globals: {
     'ts-jest': {
       diagnostics: false,
