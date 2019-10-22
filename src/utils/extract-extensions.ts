f67e40ea19afc47f15cf266cfb79858baa28525c
Kamil Kisiela <kamil.kisiela@gmail.com>
2019-10-22 13:10:05 +0200

    Allow GraphQLSchema

--- src/utils/extract-extensions.ts
+++ src/utils/extract-extensions.ts
@@ -1,4 +1,4 @@
-import { DocumentNode, Source, parse, visit, Kind } from 'graphql';
+import { DocumentNode, Source, GraphQLSchema, parse, visit, Kind, isSchema } from 'graphql';
 
 function isSource(input: any): input is Source {
   return input instanceof Source;
@@ -8,7 +8,7 @@ function isDocumentNode(input: any): input is DocumentNode {
   return input.kind === 'Document';
 }
 
-export function extractExtensions(input: DocumentNode | Source | string): DocumentNode | never {
+export function extractExtensions(input: GraphQLSchema | DocumentNode | Source | string): DocumentNode | never {
   let doc: DocumentNode;
 
   if (isDocumentNode(input)) {
@@ -17,8 +17,17 @@ export function extractExtensions(input: DocumentNode | Source | string): Docume
     doc = parse(input);
   } else if (typeof input === 'string') {
     doc = parse(input);
+  } else if (isSchema(input)) {
+    if (!input.astNode) {
+      throw new Error('GraphQLSchema has no astNode');
+    }
+
+    doc = {
+      kind: 'Document',
+      definitions: input.extensionASTNodes ? [input.astNode, ...input.extensionASTNodes] : [input.astNode],
+    };
   } else {
-    throw new Error('Only DocumentNode, Source and string are accepted');
+    throw new Error('Only GraphQLSchema, DocumentNode, Source and string are accepted');
   }
 
   const exclude = [Kind.SCALAR_TYPE_DEFINITION, Kind.OBJECT_TYPE_DEFINITION, Kind.INTERFACE_TYPE_DEFINITION, Kind.UNION_TYPE_DEFINITION, Kind.ENUM_TYPE_DEFINITION, Kind.DIRECTIVE_DEFINITION];
